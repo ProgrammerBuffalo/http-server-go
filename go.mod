764f2a1994a583050007a0369ef951aa2a704338
ProgrammerBuffalo <eldareyvazov@gmail.com>
2025-03-28 12:07:49 +0400

    feat: add yaml reader

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.21.3
 require (
 	github.com/go-chi/chi/v5 v5.2.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- storage/db.go
+++ storage/db.go
@@ -4,22 +4,38 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
+	"gopkg.in/yaml.v3"
 )
 
-const (
-	HOST     = "localhost"
-	PORT     = 5432
-	USER     = "postgres"
-	PASSWORD = "password"
-	DBNAME   = "postgres"
-)
+type DBCredential struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Name     string `yaml:"name"`
+}
 
 func DBInit() *sql.DB {
 	log.Println("Start DB initialization...")
+
+	var yamlErr error
+	yamlFile, yamlErr := os.ReadFile("../db.yaml")
+
+	if yamlErr != nil {
+		log.Fatalf("Cannot read yaml file: %s", yamlErr)
+	}
+
+	var dbCred DBCredential
+
+	if err := yaml.Unmarshal(yamlFile, &dbCred); err != nil {
+		log.Fatalf("Cannot unmarshal yaml file: %s", err)
+	}
+
 	connectionString := fmt.Sprintf("host = %s port = %d user = %s password = %s dbname = %s sslmode=disable",
-		HOST, PORT, USER, PASSWORD, DBNAME)
+		dbCred.Host, dbCred.Port, dbCred.User, dbCred.Password, dbCred.Name)
 
 	var dataSource *sql.DB
 	dataSource, err := sql.Open("postgres", connectionString)
